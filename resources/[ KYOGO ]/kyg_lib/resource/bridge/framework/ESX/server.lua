2c1110289c05b362c72c27a12d8ae6fcba00fc6c
Afdhalul Ichsan Yourdan <40620980+ShennBoku@users.noreply.github.com>
2025-09-03 22:51:36 +0700

    saving player to local resource variable

--- resources/[ KYOGO ]/kyg_lib/resource/bridge/framework/ESX/server.lua
+++ resources/[ KYOGO ]/kyg_lib/resource/bridge/framework/ESX/server.lua
@@ -4,13 +4,58 @@ if Config.Framework ~= 'esx' then return end
 ESX = exports['es_extended']:getSharedObject()
 
 AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
-    kServer.Players[tostring(playerId)] = GetPlayerName(playerId)
-    Player(playerId).state:set('isLoggedIn', true, true)
+    kServer.Players[tostring(playerId)] = {
+        id = xPlayer.ssn,
+        group = xPlayer.group,
+        source = playerId,
+        identifier = xPlayer.identifier,
+        job = {
+            name = xPlayer.job.name,
+            label = xPlayer.job.label,
+            grade = { id = xPlayer.job.grade, name = xPlayer.job.grade_name, label = xPlayer.job.grade_label },
+            salary = xPlayer.job.grade_salary,
+            isBoss = xPlayer.job.grade_isBoss or false,
+            onDuty = xPlayer.job.onDuty or false
+        },
+        gang = {
+            name = 'none',
+            label = 'None',
+            grade = { id = 0, name = 'none', label = 'None' },
+            isBoss = false
+        },
+        detail = {
+            firstName = xPlayer.variables.firstName,
+            lastName = xPlayer.variables.lastName,
+            fullname = xPlayer.name,
+            dob = xPlayer.variables.dateofbirth,
+            height = xPlayer.variables.height,
+            sex = xPlayer.variables.sex:lower(),
+            nationality = 'none',
+            phone_number = '00000000'
+        },
+        licenses = kyg.player.getIdentifiers(playerId)
+    }
+end)
+
+AddEventHandler('esx:setJob', function(playerId, newJob, lastJob)
+    if not kServer.Players[tostring(playerId)] then return end
+    kServer.Players[tostring(playerId)].job = {
+        name = newJob.name,
+        label = newJob.label,
+        grade = { id = newJob.grade, name = newJob.grade_name, label = newJob.grade_label },
+        salary = newJob.grade_salary,
+        isBoss = newJob.grade_isBoss or false,
+        onDuty = newJob.onDuty or false
+    }
+end)
+
+AddEventHandler('esx:setGroup', function(playerId, newGroup, lastGroup)
+    if not kServer.Players[tostring(playerId)] then return end
+    kServer.Players[tostring(playerId)].group = newGroup
 end)
 
 AddEventHandler('esx:playerDropped', function(playerId)
     kServer.Players[tostring(playerId)] = nil
-    Player(playerId).state:set('isLoggedIn', false, true)
 end)
 
 function kyogo.usableItem(name, cb)
--- resources/[ KYOGO ]/kyg_lib/resource/bridge/framework/QBCore/server.lua
+++ resources/[ KYOGO ]/kyg_lib/resource/bridge/framework/QBCore/server.lua
@@ -4,7 +4,59 @@ if Config.Framework ~= 'qb' then return end
 QBCore = exports['qb-core']:GetCoreObject()
 
 AddEventHandler('QBCore:Server:PlayerLoaded', function(Player)
-    kServer.Players[tostring(Player.PlayerData.source)] = GetPlayerName(Player.PlayerData.source)
+    kServer.Players[tostring(Player.PlayerData.source)] = {
+        id = Player.PlayerData.citizenid,
+        group = IsPlayerAceAllowed(Player.PlayerData.source, 'admin') and 'admin' or 'user',
+        source = Player.PlayerData.source,
+        identifier = Player.PlayerData.license,
+        job = {
+            name = Player.PlayerData.job.name,
+            label = Player.PlayerData.job.label,
+            grade = { id = Player.PlayerData.job.grade.level, name = Player.PlayerData.job.grade.name:lower(), label = Player.PlayerData.job.grade.name },
+            salary = Player.PlayerData.job.payment,
+            isBoss = Player.PlayerData.job.isboss or false,
+            onDuty = Player.PlayerData.job.onduty or false
+        },
+        gang = {
+            name = Player.PlayerData.gang.name,
+            label = Player.PlayerData.gang.label,
+            grade = { id = Player.PlayerData.gang.grade.level, name = Player.PlayerData.gang.grade.name:lower(), label = Player.PlayerData.gang.grade.name },
+            isBoss = Player.PlayerData.gang.grade.isboss or false
+        },
+        detail = {
+            firstName = Player.PlayerData.charinfo.firstname,
+            lastName = Player.PlayerData.charinfo.lastname,
+            fullname = ('%s %s'):format(Player.PlayerData.charinfo.firstname, Player.PlayerData.charinfo.lastname),
+            dob = Player.PlayerData.charinfo.birthdate,
+            height = Player.PlayerData.charinfo.height,
+            sex = Player.PlayerData.charinfo.gender == 0 and 'm' or 'f',
+            nationality = Player.PlayerData.charinfo.nationality,
+            phone_number = Player.PlayerData.charinfo.phone or '00000000'
+        },
+        licenses = kyg.player.getIdentifiers(Player.PlayerData.source)
+    }
+end)
+
+AddEventHandler('QBCore:Server:OnJobUpdate', function(Source, NewJob)
+    if not kServer.Players[tostring(Source)] then return end
+    kServer.Players[tostring(Source)].job = {
+        name = NewJob.name,
+        label = NewJob.label,
+        grade = { id = NewJob.grade.level, name = NewJob.grade.name:lower(), label = NewJob.grade.name },
+        salary = NewJob.payment,
+        isBoss = NewJob.isboss or false,
+        onDuty = NewJob.onduty or false
+    }
+end)
+
+AddEventHandler('QBCore:Server:OnGangUpdate', function(Source, NewGang)
+    if not kServer.Players[tostring(Source)] then return end
+    kServer.Players[tostring(Source)].gang = {
+        name = NewGang.name,
+        label = NewGang.label,
+        grade = { id = NewGang.grade.level, name = NewGang.grade.name:lower(), label = NewGang.grade.name },
+        isBoss = NewGang.grade.isboss or false
+    }
 end)
 
 AddEventHandler('QBCore:Server:OnPlayerUnload', function(playerId)
